822a73a3004f23cd58fa7cbaa5bb24dc42fa2b8e
kadir-zeren <k4dzeren@gmail.com>
2024-11-12 20:16:05 +0300

    for while

--- while.js
+++ while.js
@@ -15,15 +15,36 @@ console.log("BYE");
 //? 0-100'den farkli ise Kullaciniyi uyararak yeniden 0-100 arasinda
 //? not girmeye zorlayan kodu while dongusu ile yaziniz.
 
-let not = prompt("Lütfen 0-100 arasinda bir not giriniz:");
+// let not = prompt("Lütfen 0-100 arasinda bir not giriniz:");
 
 // if (not < 0 || not > 100) {
 //   console.log("Not 0'dan küçük veya 100'den büyük olamaz");
 // }
+//! -------- WHILE --------
+// while (not < 0 || not > 100) {
+//   console.log("Not 0'dan küçük veya 100'den büyük olamaz");
+//   not = prompt("Lütfen 0-100 arasinda bir not giriniz:");
+// }
 
-while (not < 0 || not > 100) {
-  console.log("Not 0'dan küçük veya 100'den büyük olamaz");
+//!------ DO - WHILE -------
+
+// let not;
+// do {
+//   not = prompt("Lütfen 0-100 arasinda bir not giriniz:");
+// //   if (not < 0 || not > 100) {
+// //     alert("Not 0'dan küçük veya 100'den büyük olamaz");
+// //   }
+// } while (not < 0 || not > 100);
+
+// console.log("Notunuz:", not);
+
+//! ------ FOR ------
+
+let not = prompt("Lütfen 0-100 arasinda bir not giriniz:");
+for (; not < 0 || not > 100; ) {
+  alert("Not 0'dan küçük veya 100'den büyük olamaz");
   not = prompt("Lütfen 0-100 arasinda bir not giriniz:");
 }
 
+//!--------------------
 console.log("Notunuz:", not);
